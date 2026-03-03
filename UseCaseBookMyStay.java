b2ed5f150d4c35f456194af475b41b07547c1761
Anir2004-star <anirban.choudhury.2004@gmail.com>
2026-03-03 15:54:15 +0530

    UC3

--- UseCaseBookMyStay.java
+++ UseCaseBookMyStay.java
@@ -1,6 +1,91 @@
 package BookMyStay;
 
+import java.util.HashMap;
+import java.util.Map;
+
 abstract class Room {
+    private String roomType;
+    private int numberOfBeds;
+    private double sizeInSqFt;
+    private double pricePerNight;
+
+    public Room(String roomType, int numberOfBeds, double sizeInSqFt, double pricePerNight) {
+        this.roomType = roomType;
+        this.numberOfBeds = numberOfBeds;
+        this.sizeInSqFt = sizeInSqFt;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getSizeInSqFt() {
+        return sizeInSqFt;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 200, 1500);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 2500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 600, 5000);
+    }
+}
+
+class RoomInventory {
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int newCount) {
+        if (inventory.containsKey(roomType)) {
+            inventory.put(roomType, newCount);
+        }
+    }
+
+    public void displayInventory() {
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
+
+public class UseCasebookMyStay {
+    public static void main(String[] args) {
+        RoomInventory inventory = new RoomInventory();
+        inventory.displayInventory();
+        inventory.updateAvailability("Single Room", 4);
+        inventory.displayInventory();
+    }
+}abstract class Room {
 
     // Encapsulated common attributes
     private String roomType;
