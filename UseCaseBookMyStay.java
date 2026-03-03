ca4361979fe063faa7ff0382367bb3bbef1e407c
Anir2004-star <anirban.choudhury.2004@gmail.com>
2026-03-03 15:47:04 +0530

    usecase 1

--- UseCaseBookMyStay.java
+++ UseCaseBookMyStay.java
@@ -1,4 +1,18 @@
 package BookMyStay;
 
 public class UseCaseBookMyStay {
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("======================================");
+        System.out.println("     Welcome to Book My Stay App     ");
+        System.out.println("     Hotel Booking System v1.0       ");
+        System.out.println("======================================");
+
+        // Application termination message
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay!");
+    }
+
 }
