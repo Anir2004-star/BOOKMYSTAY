fc80648cda32347455a5a9d269ceef8e6696c1ce
Anirban Choudhury <anirbna.choudhury.2004@gmail.com>
2026-03-11 14:40:23 +0530

    Fix nested git repository issue

---
+++ src/BookMyStay/UseCaseBookMyStay.java
@@ -0,0 +1,57 @@
+package BookMyStay;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest: " + guestName + ", Room Type: " + roomType;
+    }
+}
+
+class BookingRequestQueue {
+    private Queue<Reservation> queue;
+
+    public BookingRequestQueue() {
+        queue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        queue.offer(reservation);
+    }
+
+    public void displayQueue() {
+        for (Reservation r : queue) {
+            System.out.println(r);
+        }
+    }
+}
+
+public class UseCaseBookMyStay {
+    public static void main(String[] args) {
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        bookingQueue.addRequest(new Reservation("Anirban", "Single Room"));
+        bookingQueue.addRequest(new Reservation("Rahul", "Double Room"));
+        bookingQueue.addRequest(new Reservation("Priya", "Suite Room"));
+
+        bookingQueue.displayQueue();
+    }
+}
